63110855793382e8d78541c44fa5889b2c092189
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2022-01-13 15:11:25 +0100

    Update StepTwo.vue

--- src/views/StepTwo.vue
+++ src/views/StepTwo.vue
@@ -1,14 +1,61 @@
 <template>
-  <div></div>
+  <p>
+    If you'd have a magic wand,<br />which task you'd like to finish in
+    <span class="green-text bold">24</span> hours?
+  </p>
+  <div class="tasks">
+    <div
+      v-for="(task, index) in tasks"
+      :key="index"
+      class="task"
+      @click="chooseIndex(index)"
+    >
+      {{ task }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { Index, key } from "@/store";
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "StepTwo",
-  setup() {
-    return {};
+  setup(props, { emit }) {
+    const store = useStore(key);
+
+    const tasks = computed<string[]>(() => {
+      return store.state.tasks;
+    });
+
+    const chooseIndex = (index: Index) => {
+      store.commit("setTopTaskIndex", index);
+      emit("goNext");
+    };
+
+    return { tasks, chooseIndex };
   },
+  emits: ["goNext"],
 });
 </script>
+
+<style lang="scss" scoped>
+.tasks {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px 18px;
+
+  .task {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 20px 0;
+    transition: background-color 0.2s ease;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #ccc;
+    }
+  }
+}
+</style>

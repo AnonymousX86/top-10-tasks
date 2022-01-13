0cd64fdaed4e782f41165eaa55c76008ac03548f
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2022-01-13 13:00:46 +0100

    Create store structure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,39 @@
-import { createStore } from "vuex";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
 
-export default createStore({
-  state: {},
-  mutations: {},
+export interface State {
+  tasks: string[];
+  topTaskIndex: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | null;
+}
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export default createStore<State>({
+  state: {
+    tasks: [],
+    topTaskIndex: null,
+  },
+  mutations: {
+    setTasks(state, tasks: string[]) {
+      state.tasks = tasks;
+    },
+    clearTasks(state) {
+      state.tasks = [];
+    },
+  },
   actions: {},
   modules: {},
+  getters: {
+    stepOneDone(state): boolean {
+      return state.tasks.length === 10;
+    },
+    stepTwoDone(state): boolean {
+      return state.topTaskIndex !== null;
+    },
+    topTask(state): string | null {
+      return state.topTaskIndex === null
+        ? null
+        : state.tasks[state.topTaskIndex];
+    },
+  },
 });
